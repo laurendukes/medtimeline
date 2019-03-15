68ef2536da21ce6b3a53b7710adb81706403ae1d
laurendukes <laurendukes@google.com>
2019-03-15 19:23:35 -0400

    Update demo

--- build_demo.sh
+++ build_demo.sh
@@ -1,15 +1,15 @@
 cd medtimeline
 
 # Hide the fhir_config.js we're using so it doesn't get synthesized into the build.
-mv src/app/fhir_config.js ../fhir_config.js
-touch src/app/fhir_config.js
+#mv src/app/fhir_config.js ../fhir_config.js
+#touch src/app/fhir_config.js
 
 # Build the demo into the docs folder in the root of the repository.
-ng build --prod --output-path ../docs --base-href /medtimeline/ --configuration dev_mock_synth_data
+ng build --prod --output-path ../docs --base-href /medtimeline/
 
 # Put back the fhir_config where it belongs.
-rm src/app/fhir_config.js
-mv ../fhir_config.js src/app/fhir_config.js
+#rm src/app/fhir_config.js
+#mv ../fhir_config.js src/app/fhir_config.js
 
 # Make a new commit for the documentation.
 cd ..
