213cd6a9ebe12e7278e2c04a4fc8e7d881072341
laurendukes <laurendukes@google.com>
2019-03-15 20:44:46 -0400

    putting back ecnoutner call

--- docs/main.js
+++ docs/main.js
@@ -4750,155 +4750,30 @@ var FhirHttpService = /** @class */ (function (_super) {
      *   date range.
      */
     FhirHttpService.prototype.getEncountersForPatient = function (dateRange) {
+        var _this = this;
         var queryParams = {
             type: _constants__WEBPACK_IMPORTED_MODULE_2__["FhirResourceType"].Encounter,
         };
         if (!dateRange) {
             dateRange = _constants__WEBPACK_IMPORTED_MODULE_2__["APP_TIMESPAN"];
         }
-        return Promise.resolve([new _fhir_data_classes_encounter__WEBPACK_IMPORTED_MODULE_6__["Encounter"]({
-                'class': 'inpatient',
-                'id': '4027918',
-                'identifier': [{
-                        'period': { 'start': '2016-06-22T21:06:58.000Z' },
-                        'system': 'urn:oid:3.3.3.3.3.3',
-                        'type': { 'text': 'FIN NBR' },
-                        'use': 'usual',
-                        'value': '20003412'
-                    }],
-                'location': [{
-                        'location': {
-                            'display': 'Baseline East Medical Center, BE Hospital, 1N, 0152, A',
-                            'reference': 'Location/35646785'
-                        },
-                        'status': 'completed'
-                    }],
-                'meta': { 'lastUpdated': '2016-06-27T15:01:18.000Z', 'versionId': '1' },
-                'participant': [
-                    {
-                        'individual': { 'display': 'Who, Doctor', 'reference': 'Practitioner/1' },
-                        'period': { 'start': '2016-11-09T18:07:16.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'ADM',
-                                        'display': 'admitter',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Admitting Physician'
-                            }]
-                    },
-                    {
-                        'individual': { 'display': 'Song, River', 'reference': 'Practitioner/2048007' },
-                        'period': { 'start': '2016-11-09T15:44:00.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'ADM',
-                                        'display': 'admitter',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Admitting Physician'
-                            }]
-                    },
-                    {
-                        'individual': {
-                            'display': 'Strange, Doctor',
-                            'reference': 'Practitioner/1912007'
-                        },
-                        'period': { 'start': '2016-06-22T21:06:58.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'ADM',
-                                        'display': 'admitter',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Admitting Physician'
-                            }]
-                    },
-                    {
-                        'individual': {
-                            'display': 'Strange, Doctor',
-                            'reference': 'Practitioner/1912007'
-                        },
-                        'period': { 'start': '2016-06-22T21:06:58.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'ATND',
-                                        'display': 'attender',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Attending Physician'
-                            }]
-                    },
-                    {
-                        'individual': {
-                            'display': 'Strange, Doctor',
-                            'reference': 'Practitioner/1912007'
-                        },
-                        'period': { 'start': '2016-06-27T15:03:36.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'PPRF',
-                                        'display': 'primary performer',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Ordering Physician'
-                            }]
-                    },
-                    {
-                        'individual': {
-                            'display': 'Frankenstein, Doctor',
-                            'reference': 'Practitioner/1590015'
-                        },
-                        'period': {
-                            'end': '2016-06-23T18:34:38.000Z',
-                            'start': '2016-06-23T18:34:38.000Z'
-                        },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'PART',
-                                        'display': 'Participation',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Other'
-                            }]
-                    },
-                    {
-                        'individual': {
-                            'display': 'Strange, Doctor',
-                            'reference': 'Practitioner/1912007'
-                        },
-                        'period': { 'start': '2016-06-22T21:06:58.000Z' },
-                        'type': [{
-                                'coding': [{
-                                        'code': 'REF',
-                                        'display': 'referrer',
-                                        'system': 'http://hl7.org/fhir/v3/ParticipationType',
-                                        'userSelected': false
-                                    }],
-                                'text': 'Referring Physician'
-                            }]
-                    }
-                ],
-                'patient': { 'display': 'Smart, Joe', 'reference': 'Patient/4342010' },
-                'period': {
-                    'start': '2018-08-01T06:00:00.000Z',
-                    'end': '2018-10-10T06:00:00.000Z'
-                },
-                'resourceType': 'Encounter',
-                'serviceProvider': { 'reference': 'Organization/619848' },
-                'status': 'finished',
-                'text': {
-                    'div': '<div><p><b>Encounter</b></p><p><b>Location</b>: Baseline East Medical Center, BE Hospital, 1N, 0152, A</p><p><b>Type</b>: Inpatient</p><p><b>Status</b>: Discharged</p><p><b>Period Start Date</b>: Wed Jun 22 21:05:00 2016 (UTC)</p><p><b>Attending Physician</b>: Strange, Doctor</p></div>',
-                    'status': 'generated'
-                },
-                'type': [{ 'text': 'Inpatient' }]
-            })]);
+        // The Cerner implementation of the Encounter search does not offer any
+        // filtering by date at this point, so we grab all the encounters
+        // then filter them.
+        return this.smartApiPromise.then(function (smartApi) { return smartApi.patient.api.fetchAll(queryParams)
+            .then(function (results) {
+            results =
+                results
+                    .map(function (result) {
+                    return new _fhir_data_classes_encounter__WEBPACK_IMPORTED_MODULE_6__["Encounter"](result);
+                })
+                    .filter(function (encounter) {
+                    return dateRange.intersection(encounter.period) !== null;
+                });
+            return results;
+        }, function (rejection) {
+            _this.debugService.logError(rejection);
+        }); });
     };
     /**
      * Saves the current HTML of the graphs rendered as a DocumentReference
