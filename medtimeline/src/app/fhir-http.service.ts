4f69a591858fb19be1bcb94aa52fcd1f720046b2
laurendukes <laurendukes@google.com>
2019-03-15 20:01:25 -0400

    http

--- medtimeline/src/app/fhir-http.service.ts
+++ medtimeline/src/app/fhir-http.service.ts
@@ -106,43 +106,7 @@ export class FhirHttpService extends FhirService {
   getMedicationAdministrationsWithCode(
       code: RxNormCode, dateRange: Interval,
       limitCount?: number): Promise<MedicationAdministration[]> {
-    const queryParams = {
-      type: FhirResourceType.MedicationAdministration,
-      query: {
-        effectivetime: {
-          $and: [
-            GREATER_OR_EQUAL + dateRange.start.toISO(),
-            LESS_OR_EQUAL + dateRange.end.toISO()
-          ]
-        },
-        medication: {
-          code: RxNormCode.CODING_STRING + '|' + code.codeString,
-        }
-      }
-    };
-
-    if (limitCount) {
-      queryParams.query['_count'] = limitCount;
-    }
-
-    return this.smartApiPromise.then(
-        smartApi => smartApi.patient.api.fetchAll(queryParams)
-                        .then(
-                            (results: any[]) => results.map(result => {
-                              try {
-                                return new MedicationAdministration(result);
-                              } catch (e) {
-                                this.debugService.logError(e);
-                                throw e;
-                              }
-                            }),
-                            // Do not return any MedicationAdministrations for
-                            // this code if one of the MedicationAdministration
-                            // constructions throws an error.
-                            rejection => {
-                              this.debugService.logError(rejection);
-                              throw rejection;
-                            }));
+    return Promise.resolve([]);
   }
 
   /**
@@ -212,28 +176,8 @@ export class FhirHttpService extends FhirService {
     if (!dateRange) {
       dateRange = APP_TIMESPAN;
     }
-    // The Cerner implementation of the Encounter search does not offer any
-    // filtering by date at this point, so we grab all the encounters
-    // then filter them.
 
-    return this.smartApiPromise.then(
-        smartApi => smartApi.patient.api.fetchAll(queryParams)
-                        .then(
-                            (results: any[]) => {
-                              results =
-                                  results
-                                      .map(result => {
-                                        return new Encounter(result);
-                                      })
-                                      .filter(
-                                          encounter =>
-                                              dateRange.intersection(
-                                                  encounter.period) !== null);
-                              return results;
-                            },
-                            rejection => {
-                              this.debugService.logError(rejection);
-                            }));
+    return Promise.resolve([]);
   }
 
   /**
